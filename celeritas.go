61fdfa3a6d3071b9c67b9c529f94ee8e759f0138
Feng Guo <fengsterooni@yahoo.com>
2022-07-11 18:17:11 -0700

    Cache and tests

--- celeritas.go
+++ celeritas.go
@@ -10,10 +10,13 @@ import (
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/alexedwards/scs/v2"
+	"github.com/fengsterooni/celeritas/cache"
 	"github.com/fengsterooni/celeritas/render"
 	"github.com/fengsterooni/celeritas/session"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+
+	"github.com/gomodule/redigo/redis"
 )
 
 const verion = "1.0.0"
@@ -32,6 +35,7 @@ type Celeritas struct {
 	JetViews      *jet.Set
 	config        config
 	EncryptionKey string
+	Cache         cache.Cache
 }
 
 type config struct {
@@ -40,6 +44,7 @@ type config struct {
 	cookie       cookieConfig
 	sesseionType string
 	datebase     databaseConfig
+	redis        redisConfig
 }
 
 func (c *Celeritas) New(rootPath string) error {
@@ -81,6 +86,11 @@ func (c *Celeritas) New(rootPath string) error {
 		}
 	}
 
+	if os.Getenv("CACHE") == "redis" {
+		myRedisCache := c.createClientRedisCache()
+		c.Cache = myRedisCache
+	}
+
 	c.InfoLog = infoLog
 	c.ErrorLog = errorLog
 	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
@@ -102,6 +112,11 @@ func (c *Celeritas) New(rootPath string) error {
 		datebase: databaseConfig{
 			database: os.Getenv("DATABASE_TYPE"),
 			dsn:      c.BuildDSN(),
+		},
+		redis: redisConfig{
+			host:     os.Getenv("REDIS_HOST"),
+			password: os.Getenv("REDIS_PASSWORD"),
+			prefix:   os.Getenv("REDIS_PREFIX"),
 		},
 	}
 
@@ -187,6 +202,33 @@ func (c *Celeritas) createRenderer() {
 	}
 
 	c.Render = &myRender
+}
+
+func (c *Celeritas) createClientRedisCache() *cache.RedisCache {
+	cacheClient := cache.RedisCache{
+		Conn:   c.createRedisPool(),
+		Prefix: c.config.redis.prefix,
+	}
+
+	return &cacheClient
+}
+
+func (c *Celeritas) createRedisPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     50,
+		MaxActive:   10000,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp",
+				c.config.redis.host,
+				redis.DialPassword(c.config.redis.password))
+		},
+
+		TestOnBorrow: func(conn redis.Conn, t time.Time) error {
+			_, err := conn.Do("PING")
+			return err
+		},
+	}
 }
 
 func (c *Celeritas) BuildDSN() string {
