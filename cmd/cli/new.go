26c727b985f462a6e3bbcf62ae08e29dc1515033
Feng Guo <fengsterooni@yahoo.com>
2022-07-21 23:19:26 -0700

    Changed to http, and git clone worked!

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -30,7 +30,8 @@ func doNew(appName string) {
 	// git clone the skeleton application
 	color.Green("\tCloning repository...")
 	_, err := git.PlainClone("./"+appName, false, &git.CloneOptions{
-		URL:      "git@github.com:fengsterooni/celeritas-app.git",
+		// URL:      "git@github.com:fengsterooni/celeritas-app.git",
+		URL:      "https://github.com/fengsterooni/celeritas-app.git",
 		Progress: os.Stdout,
 		Depth:    1,
 	})
